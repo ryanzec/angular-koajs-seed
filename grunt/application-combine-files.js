b7bf3fa1bb35090f6319f5cce2334d1077a63744
Ryan Zec <code@ryanzec.com>
2014-03-06 05:46:33 -0500

    updates for coding standards

--- grunt/application-combine-files.js
+++ grunt/application-combine-files.js
@@ -7,5 +7,5 @@ module.exports = [
   'app/components/core/module.js',
   'app/components/core/template-interceptor.js',
   'app/components/home/module.js',
-  'app/components/home/home.js'
+  'app/components/home/home-controller.js'
 ];
